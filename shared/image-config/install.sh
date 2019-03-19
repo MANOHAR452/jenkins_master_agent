3b0fc3038531d31d79a72d500aa611ff73b0c1ea
Manohar Reddy A <a.manohar452@gmail.com>
2019-03-18 23:59:03 -0700

    Update install.sh

--- shared/image-config/install.sh
+++ shared/image-config/install.sh
@@ -52,19 +52,19 @@ gcloud config configurations create packer --no-activate
 ### Install Docker - https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
 
 # Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-# Add Docker repositories
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" >> /etc/apt/sources.list.d/additiona
-l-repositories.list
-echo "deb http://ftp-stud.hs-esslingen.de/ubuntu xenial main restricted universe multiverse" >> /etc/apt/sources.li
-st.d/official-package-repositories.list
-
-# Add keyserver
-apt-key adv --no-tty --keyserver keyserver.ubuntu.com --recv-keys 437D05B5
-
-# Update repository and install Docker-CE
-apt-get update && apt-get install -y docker-ce
+apt-get update && \
+apt-get -y install apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common && \
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey && \
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable" && \
+apt-get update && \
+apt-get -y install docker-ce
 
 ### Install Packer
 cd /usr/local/bin
